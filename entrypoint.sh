593f295c9a609011c2ea2e4693448e3bd10eec5c
JangwanKO <Jangwanko93@gmail.com>
2025-07-25 02:46:39 +0900

    migrate

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,9 @@ while ! nc -z db 5432; do
 done
 echo "PostgreSQL started 🐘"
 
+echo "Running migrations..."
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+echo "Starting Gunicorn..."
 exec gunicorn myflix.wsgi:application --bind 0.0.0.0:8000 --workers 3
 
