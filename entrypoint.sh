d3c3cd0abd4b37eb5cf2be3c088274a7b734b640
Jangwan-Laptop <Jangwanko93@gmail.com>
2025-10-20 20:41:19 +0900

    openstack

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 echo "Waiting for PostgreSQL..."
-while ! nc -z db 5432; do
+while ! nc -z $DB_HOST $DB_PORT; do
   sleep 1
 done
 echo "PostgreSQL started 🐘"
 
-exec gunicorn myflix.wsgi:application --bind 0.0.0.0:8000 --workers 3
+echo "Running migrations..."
+python manage.py migrate
 
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+exec gunicorn myflix.wsgi:application --bind 0.0.0.0:8000 --workers 3
